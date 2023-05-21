3fed56b59ce27463c4723b326c8d49db04bc355e
Juan Pablo Acosta Cuero <pabloacosta_999@hotmail.com>
2023-05-20 19:06:59 -0500

    Edit Patient

---
+++ Pages/Patients/Edit.cshtml.cs
@@ -0,0 +1,69 @@
+using Ciudadanos_Sanos.Data;
+using Ciudadanos_Sanos.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ciudadanos_Sanos.Pages.Patients
+{
+    public class EditModel : PageModel
+    {
+        private readonly CSContext _context;
+
+        public EditModel(CSContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Patient Patient { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Patients == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Patients.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            Patient = category;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Patient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(Patient.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool PatientExists(int id)
+        {
+            return (_context.Patients?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
