61c8e6ea1dd292529cc53c1f9c6463b09b799663
Juan Pablo Acosta Cuero <pabloacosta_999@hotmail.com>
2023-05-20 19:17:13 -0500

    Create Doctor

---
+++ Pages/Doctors/Create.cshtml.cs
@@ -0,0 +1,36 @@
+using Ciudadanos_Sanos.Data;
+using Ciudadanos_Sanos.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ciudadanos_Sanos.Pages.Doctors
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CSContext _context;
+        public CreateModel(CSContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Doctor Doctor { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Doctors == null || Doctor == null)
+            {
+                return Page();
+            }
+            _context.Doctors.Add(Doctor);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
