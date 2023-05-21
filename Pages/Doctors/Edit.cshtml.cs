4099a5a93202ac716e5bd334810e046ce654c04b
Juan Pablo Acosta Cuero <pabloacosta_999@hotmail.com>
2023-05-20 19:23:35 -0500

    Edit Doctor

---
+++ Pages/Doctors/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+using Ciudadanos_Sanos.Data;
+using Ciudadanos_Sanos.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ciudadanos_Sanos.Pages.Doctors
+{
+    [Authorize]
+    public class EditModel : PageModel
+    {
+        private readonly CSContext _context;
+
+        public EditModel(CSContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Doctor Doctor { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Doctors == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Doctors.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            Doctor = category;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Doctor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorExists(Doctor.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DoctorExists(int id)
+        {
+            return (_context.Doctors?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
